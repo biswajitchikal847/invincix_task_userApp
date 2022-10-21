17fe1295c99bb96a4ba15947a272c84e810e1970
Biswajit Biswal <biswajitchikal847@gmail.com>
2022-10-21 23:25:37 +0530

    .

--- src/Components/UserDetails.jsx
+++ src/Components/UserDetails.jsx
@@ -4,28 +4,15 @@ import { useParams } from "react-router-dom";
 import "./UserDetails.css"
 import { AiOutlineMail } from "react-icons/ai";
 const UserDetails = () => {
-    const {id} = useParams();
+    const { id } = useParams();
     const [user, setuser] = useState([]);
-    const [loading,setLoading] = useState(false);
+    const [loading, setLoading] = useState(false);
+
     
-    // const getuser = async () => {
-    //     try{
-    //         setLoading(true);
-    //         const url = `https://reqres.in/api/users/${id}`;
-    //         let res = await fetch(url);
-    //         let data = await res.json();
-    //         console.log(data)
-    //         setuser(data);
-    //         setLoading(false);
-    //     }catch(e){
-    //         console.log(e)
-    //     }
-     
-    // }
 
     useEffect(() => {
         const getuser = async () => {
-          try{
+            try {
                 setLoading(true);
                 const url = `https://reqres.in/api/users/${id}`;
                 let res = await fetch(url);
@@ -33,46 +20,46 @@ const UserDetails = () => {
                 console.log(data)
                 setuser(data);
                 setLoading(false);
-          }catch(e){
-            console.log(e)
-          }
-          
-         
+            } catch (e) {
+                console.log(e)
+            }
+
+
         }
-getuser();
-    },[])
-const ShowUserDetails = () =>{
+        getuser();
+    }, [])
+    const ShowUserDetails = () => {
+        return (
+            <>
+
+                {user.data ?
+                    <div id="details_maindiv">
+                        <div>
+                            <div>
+                                <img src={user.data.avatar} alt="" />
+                                {console.log(user)}
+                            </div>
+                            <div>
+                                <p>{user.data.first_name} {user.data.last_name}</p>
+                                <div id="hrtag">
+                                    <hr />
+
+                                </div>
+                                <p>Contact :-</p>
+                                <span><AiOutlineMail /></span><i>{user.data.email}</i>
+                            </div>
+                        </div>
+                    </div>
+                    : "Loading"}
+
+            </>
+        )
+    }
     return (
         <>
-           
-            {user.data ?  
-             <div id="details_maindiv">
-                <div>
-                    <div>
-                        <img src={user.data.avatar} alt="" />
-                        {console.log(user)}
-                    </div>
-                    <div>
-                        <p>{user.data.first_name} {user.data.last_name}</p>
-                        <div id="hrtag">
-                        <hr />
-                        
-                        </div>
-                        <p>Contact :-</p>
-                        <span><AiOutlineMail/></span><i>{user.data.email}</i>
-                    </div>
-                </div>
-             </div> 
-             : "Loading"}
-
-        </>
-    )
-}
-    return (
-        <> 
-        <div id="nav">
-        <div>User Details</div>
-    </div>
+            <div id="nav">
+                <div>User Details</div>
+            </div>
             {loading ? "Loading" : <ShowUserDetails />}
         </>
     )
